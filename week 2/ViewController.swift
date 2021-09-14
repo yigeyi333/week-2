26277f5f7d914ff20ccec56e8d116c6aba112e1e
Yi Han <Yi Han>
2021-09-14 13:06:09 -0400

    Initial Commit

---
+++ week 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week 2
+//
+//  Created by 韩熠 on 9/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
